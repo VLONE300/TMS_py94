a478b6946badbc7a1469b21e67118d99a928e8c9
VLONE <ermolich158@gmail.com>
2023-12-03 10:42:02 +0300

    init self work for lesson13

--- self_works/lesson13/static.py
+++ self_works/lesson13/static.py
@@ -1,53 +1,24 @@
-# # class Example:
-# #     @staticmethod
-# #     def hello():
-# #         print('hello')
-# #
-# #     def hello2(self):
-# #         print('hello2')
-# import json
-#
-#
-# class Example:
-#     @classmethod
-#     def class_hello(cls):
-#         return f'class_hello{cls}'
-#
-#
-# print(Example.class_hello())
-#
-# qw = Example()
-# print(qw.class_hello())
 import json
-
 from self_works.lesson13.custom_exceptions import ConnectionRefused
 
 
 class DataStorage:
-    def __init__(self, file=None):
-        self.file = file
+    file = None
 
-
-
-    def connect_or_create(self, path):
+    @classmethod
+    def connect_or_create(cls, path):
         try:
-            with open(path, 'r') as file:
-                json.load(path)
-                self.file = file
+            cls.file = open(path, 'r+')
         except FileNotFoundError:
-            with open('test.json', 'w') as file:
-                json.dump('111',file)
-                self.file = file
+            cls.file = open(path, 'w')
 
-    def read(self):
-        if self.file:
-            return self.file.read()
-        raise ConnectionRefused('error')
-
+    @classmethod
+    def read(cls):
+        if cls.file:
+            return cls.file.read()
+        raise ConnectionRefused('first you need to call the method connect_or_create')
 
 
-
-
-
-q = DataStorage()
-q.connect_or_create('self_works/lesson13/test.json')
+DataStorage.connect_or_create('example.json')
+data = DataStorage.read()
+print(data)
