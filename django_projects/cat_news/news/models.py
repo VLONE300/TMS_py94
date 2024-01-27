bffb9e49c9b742cd67def81c629bf449cbd429ea
VLONE <ermolich158@gmail.com>
2024-01-27 12:42:22 +0300

    models and templates

--- django_projects/cat_news/news/models.py
+++ django_projects/cat_news/news/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class News(models.Model):
-    image = models.ImageField(upload_to='cat_news/images/', blank=True)
+    image = models.ImageField(upload_to='', blank=True)
     title = models.CharField(max_length=128, null=False, blank=False)
     content = models.TextField(null=False, blank=False)
     likes = models.IntegerField(default=0)
@@ -18,12 +18,12 @@ class News(models.Model):
 
 
 class Comment(models.Model):
-    nick_name = models.CharField(max_length=128)
+    nick_name = models.CharField(max_length=255)
     content = models.TextField(max_length=255)
     date = models.DateField(auto_now_add=True)
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
-    news = models.ForeignKey('News', on_delete=models.CASCADE, related_name='comments')
+    news = models.ForeignKey('News', on_delete=models.CASCADE, related_name='comments_for_news')
     parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='replies', null=True, blank=True)
 
     def __str__(self):
--- django_projects/cat_news/news/views.py
+++ django_projects/cat_news/news/views.py
@@ -2,8 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
+from .models import News
+
 
 # Create your views here.
 class NewsIndexView(View):
+    news = News.objects.all().order_by('-date')
+
     def get(self, request):
-        return HttpResponse("Hello, world")
+        return render(request, 'news/index.html', context={"news": self.news})
